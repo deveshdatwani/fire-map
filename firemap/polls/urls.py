ac52668ae4afcfc9f12199c76effcf2599190de8
deveshdatwani <datwanidevesh@gmail.com>
2023-09-23 15:11:12 -0400

    remove uneccesary items

--- firemap/polls/urls.py
+++ firemap/polls/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("hello/", views.hello, name="hello"),
-    path("question/<int:question_id>/", views.question, name="question")
 ]
--- firemap/polls/views.py
+++ firemap/polls/views.py
@@ -6,11 +6,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "map.html")
-
-
-def hello(reques):
-    return HttpResponse("Hello world!")
-
-
-def question(request, question_id):
-    return HttpResponse(f'The question id is {question_id}')
