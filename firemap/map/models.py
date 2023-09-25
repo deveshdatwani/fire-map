ee788f2bf065fb73921826cff506dd60634fe0e1
deveshdatwani <datwanidevesh@gmail.com>
2023-09-24 20:29:47 -0400

    fire table

--- firemap/map/models.py
+++ firemap/map/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+class Fire(models.Model):
+    idx = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    img = models.ImageField(null=True)   
+
+    def __str__(self):
+        return f'{self.idx}'
